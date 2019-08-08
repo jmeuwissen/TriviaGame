e86b62bec414af6b6c212cefc61192947b2f1502
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-08-07 22:19:58 -0500

    further fleshed out psuedocode. found algorithm for shuffling elements of an array for randomizing answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,18 @@ fetch(
     jsonTriviaQuestions = responseJson;
 });
 
+function displayQuestion(){
+
+}
+
+function nextQuestionTimeout(){
+    transitionTimeout = setTimeout(nextQuestion, 3000);
+}
+
+function assignAnswers(){
+    
+}
+
 //start the main screen
 //display will have text for user to start the game
 
@@ -25,7 +37,27 @@ fetch(
 //place question in the question div
 //place each answer in a new element and append it to the answer div
 //randomize the position of the correct answer
+//  
+//  randomize the order in which the answer elements are appended to the page
+
+
+//  push each element into and array and use
+//  use 
+// function shuffle(array) {
+//   array.sort(() => Math.random() - 0.5);
+// }
+
+//nextquestion
+//assign new content to each answer element
+//correct answer posiiton needs to be randomized again.
+
 //assign event listeners to each wrong answer
-//  listener will call the incorrectlyAnswered function
+//  listener will set message to "wrong answer!" and call nextQuestionTimeout()
+
+
 //assign event listeners to the right answer
-//set a timeout to trigger the incorrectlyAnswered function
+//  listener will set message to "correct answer!" and call nextQuestionTimeout()
+
+
+//set a timeout to trigger after ten seconds
+//  sets message to "You took too long" calls nextQuestionTimeout
