eb28be8fb6285b58e97773cb531f94dcde1b53d7
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-08-08 22:22:42 -0500

    implemented removal of even listeners. rage click away!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,41 @@
 
-let jsonTriviaQuestions = null; 
+let jsonTriviaQuestions = null;
 let questionNumber = 0;
 let transitionTimeout = null;
 let tickingAnswerTimeout = null;
 let correctCount = 0;
 let incorrectCount = 0;
-//grabbing a bunch of trivia questions
-fetch(
-    "https://opentdb.com/api.php?amount=15&type=multiple"
-).then(function (response) {
-    return response.json();
-}).then(function (responseJson) {
-    console.log("fetched");
-    jsonTriviaQuestions = responseJson;
-    displayQuestion();
-});
+let timeLeft = 10;
+let transitionTimeoutSet = false;
+
+    //grabbing a bunch of trivia questions
+    fetch(
+        "https://opentdb.com/api.php?amount=3&type=multiple"
+    ).then(function (response) {
+        return response.json();
+    }).then(function (responseJson) {
+        console.log("fetched");
+        jsonTriviaQuestions = responseJson;
+        displayQuestion();
+    });
 
 //displays the next question and/or starts the game
-function displayQuestion(){
+function displayQuestion() {
     document.getElementById("question").innerHTML = jsonTriviaQuestions.results[questionNumber].question;
     assignAnswers();
     tickingAnswerTimeout = setTimeout(answerIncorrectly, 10000);
     clearTimeout(transitionTimeout);
+    transitionTimeoutSet = false;
 }
 
-function nextQuestionTimeout(){
-    transitionTimeout = setTimeout(nextQuestion, 3000);
+function nextQuestionTimeout() {
+    if (!transitionTimeoutSet) {
+        transitionTimeoutSet = true;
+        transitionTimeout = setTimeout(nextQuestion, 3000);
+    }
 }
 
-function endGame(){
+function endGame() {
     //display end-game stats
     document.getElementById("question").innerHTML = "";
     numberCorrectEl = document.createElement("p");
@@ -45,70 +52,91 @@ function endGame(){
 
     const displayEl = document.getElementById("display");
 
-    displayEl.append(numberCorrectEl,numberIncorrectEl,resetButtonEl);
+    displayEl.append(numberCorrectEl, numberIncorrectEl, resetButtonEl);
 
 
 
 }
 
-function nextQuestion(){
-    
+function nextQuestion() {
+
     document.getElementById("choices").innerHTML = "";
-    questionNumber++;   
+    questionNumber++;
     document.getElementById("message").textContent = "";
-    if(questionNumber === jsonTriviaQuestions.results.length){
+    if (questionNumber === jsonTriviaQuestions.results.length) {
         endGame();
     }
-    else{
+    else {
         displayQuestion();
     }
 }
 
-function answerCorrectly(){
+
+function removeAnswerListeners(){
+    //remove listener from correct answer div
+    document.getElementById("correct-answer").removeEventListener("click", answerCorrectly);
+
+
+    //remove even listener from each incorrect answer div
+    const incorrectEls = document.getElementsByClassName("incorrect-answer");
+
+    //because i guess getelementsbyclassname returns an "array-like" object so no forEach!
+    for (let i = 0; i < incorrectEls.length; i++) {
+        console.log("removedListener!")
+        incorrectEls.item(i).removeEventListener("click", answerIncorrectly);
+        
+    }
+}
+
+function answerCorrectly() {
     correctCount++;
     document.getElementById("message").textContent = "correct!"
     clearTimeout(tickingAnswerTimeout);
+
+    removeAnswerListeners();
     nextQuestionTimeout();
 }
 
-function answerIncorrectly(){
+function answerIncorrectly() {
     incorrectCount++;
     document.getElementById("correct-answer").textContent += " Correct Answer";
     document.getElementById("message").textContent = "Wrong Answer/You Took Too Long!"
     clearTimeout(tickingAnswerTimeout);
+    removeAnswerListeners();
     nextQuestionTimeout();
 }
 
-function assignAnswers(){
-    
+function assignAnswers() {
+
     //putting all answers into an array from the json
     const answers = jsonTriviaQuestions.results[questionNumber].incorrect_answers;
     answers.push(jsonTriviaQuestions.results[questionNumber].correct_answer);
-    
+
     //stack exchange says this shuffles an array
     answers.sort(() => Math.random() - 0.5);
     answers.forEach(answer => {
-        choicesEl = document.getElementById("choices");
+        const choicesEl = document.getElementById("choices");
         const choiceEl = document.createElement("p");
         choiceEl.innerHTML = answer;
 
         //if the element is the correct answer, this gives the element an event listener with function handling a correct answer
-        if(answer === jsonTriviaQuestions.results[questionNumber].correct_answer ){
+        if (answer === jsonTriviaQuestions.results[questionNumber].correct_answer) {
             choiceEl.addEventListener("click", answerCorrectly);
             choiceEl.setAttribute("id", "correct-answer");
         }
         //otherwise the element is given a listener with function handling incorrect answer
-        else{
+        else {
             choiceEl.addEventListener("click", answerIncorrectly);
+            choiceEl.setAttribute("class", "incorrect-answer");
         }
         choicesEl.append(choiceEl);
     });
-    
+
 }
 
-function resetGame(){
+function resetGame() {
     questionNumber = 0;
-        
+
     correctCount = 0;
     incorrectCount = 0;
 
